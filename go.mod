8348e5060312b33dfa39da083537e3ae57d11dd3
univana <univana@outlook.com>
2020-05-26 18:01:45 +0800

    feat: 实现基本的文件上传功能

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/astaxie/beego v1.12.1
+	github.com/iris-contrib/go.uuid v2.0.0+incompatible
 	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
 )
--- routers/router.go
+++ routers/router.go
@@ -8,4 +8,8 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{}, "*:Get")
+	beego.Router("/equalize", &controllers.MainController{}, "*:Equalize")
+
+	//图片上传
+	beego.Router("/api/upload", &controllers.MainController{}, "post:Upload")
 }
